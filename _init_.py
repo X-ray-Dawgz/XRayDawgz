35a5d5da686319f31cf4f70125b12e5942ad1d2c
Cheng-yuan-Wu <59403132+Cheng-yuan-Wu@users.noreply.github.com>
2020-03-10 01:19:17 -0700

    init

--- _init_.py
+++ _init_.py
@@ -6,35 +6,6 @@ from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
 from keras import backend as K
- 
-class LeNet:
-    @staticmethod
-    def build(width, height, depth, classes):
-        # initialize the model
-        model = Sequential()
-        inputShape = (height, width, depth)
-        # if we are using "channels last", update the input shape
-        if K.image_data_format() == "channels_first":   #for tensorflow
-            inputShape = (depth, height, width)
-        # first set of CONV => RELU => POOL layers
-        model.add(Conv2D(20, (5, 5),padding="same",input_shape=inputShape))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-        #second set of CONV => RELU => POOL layers
-        model.add(Conv2D(50, (5, 5), padding="same"))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-        # first (and only) set of FC => RELU layers
-        model.add(Flatten())
-        model.add(Dense(500))
-        model.add(Activation("relu"))
-
-        # softmax classifier
-        model.add(Dense(classes))
-        model.add(Activation("softmax"))
-
-        # return the constructed network architecture
-        return model
 
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
@@ -56,19 +27,3 @@ import os
 import sys
 sys.path.append('..')
 from net.lenet import LeNet
-
-
-
-def args_parse():
-    # construct the argument parse and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-dtest", "--dataset_test", required=True,
-        help="path to input dataset_test")
-    ap.add_argument("-dtrain", "--dataset_train", required=True,
-        help="path to input dataset_train")
-    ap.add_argument("-m", "--model", required=True,
-        help="path to output model")
-    ap.add_argument("-p", "--plot", type=str, default="plot.png",
-        help="path to output accuracy/loss plot")
-    args = vars(ap.parse_args()) 
-    return args
