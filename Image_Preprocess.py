7345d21ea63e1504286d1cc12613d43fbdb992eb
Rick Hsiao <yhsiao13@uw.edu>
2020-03-14 15:24:08 -0700

    cut image

--- Image_Preprocess.py
+++ Image_Preprocess.py
@@ -1,43 +1,47 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[40]:
-
-
 import numpy as np
 from PIL import Image
 import os
 
-
-# In[88]:
+def is_img(ext):
+    ext = ext.lower()
+    if ext == '.jpg':
+        return True
+    elif ext == '.png':
+        return True
+    elif ext == '.jpeg':
+        return True
+    elif ext == '.bmp':
+        return True
+    else:
+        return False
 
 
 def BCC_test():
     BCC_test_tmp=[]
-    for file in os.listdir("Images/BCC_test"):
+    for x in os.listdir("Images/BCC_test"):
+        
+        if is_img(osp.splitext(x)[1]):
+        
         img = Image.open("Images/BCC_test"+"/"+file)
         image_file = img.convert('L')
         img_convert_ndarray = np.array(image_file)
         BCC_test_tmp.append(img_convert_ndarray)
     return BCC_test_tmp
-    
-
-
-# In[89]:
+   
 
 
 def FCC_test():
     FCC_test_tmp=[]
     for file in os.listdir("Images/FCC_test"):
-        img = Image.open("Images/FCC_test"+"/"+file)
-        image_file = img.convert('L')
-        img_convert_ndarray = np.array(image_file)
-        FCC_test_tmp.append(img_convert_ndarray)
+        if file == ".DS_Store":
+            pass
+        else:
+            img = Image.open("Images/FCC_test"+"/"+file)
+            image_file = img.convert('L')
+            img_convert_ndarray = np.array(image_file)
+            FCC_test_tmp.append(img_convert_ndarray)
     return FCC_test_tmp
-
-
-# In[90]:
-
+   
 
 def BCC_train():
     BCC_train_tmp=[]
@@ -50,9 +54,6 @@ def BCC_train():
             img_convert_ndarray = np.array(image_file)
             BCC_train_tmp.append(img_convert_ndarray)
     return BCC_train_tmp
-
-
-# In[91]:
 
 
 def FCC_train():
@@ -68,7 +69,6 @@ def FCC_train():
     return FCC_train_tmp
 
 
-# In[ ]:
 
 
 
