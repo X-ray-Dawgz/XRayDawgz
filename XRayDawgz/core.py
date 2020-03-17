87aea57331fcfdce9ec0f5107d9ddd29e07dc0d4
Cheng-yuan-Wu <59403132+Cheng-yuan-Wu@users.noreply.github.com>
2020-03-17 14:27:17 -0700

    Update core.py

--- XRayDawgz/core.py
+++ XRayDawgz/core.py
@@ -1,8 +1,7 @@
+from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-
 import numpy as np
 
 # image size
