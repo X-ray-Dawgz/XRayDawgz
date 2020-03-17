c8d472cf162e114f35acfb24b8a64ba96a1a2c3a
Cheng-yuan-Wu <59403132+Cheng-yuan-Wu@users.noreply.github.com>
2020-03-17 14:06:18 -0700

    Update core.py

--- XRayDawgz/core.py
+++ XRayDawgz/core.py
@@ -1,12 +1,9 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-from keras import backend as K
-from keras.preprocessing import image
 
 import numpy as np
-import os
 
 # image size
 img_width, img_height = 379,288
