5a7b04dd3a333e3dca371f921ade532a92a9ae9c
Robert Biegaj <59549876+robertbiegaj@users.noreply.github.com>
2020-03-18 04:34:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xraydawgz",
-    version="0.2",
+    version="0.3",
     license='MIT',
     author="Rick, Alex, Robert, Kevin",
     author_email = 'rbiegaj@uw.edu',
