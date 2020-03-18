8400128519e5bf34a082cfc056684617550e2edf
Robert Biegaj <59549876+robertbiegaj@users.noreply.github.com>
2020-03-18 03:13:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-ssetup(
+setup(
     name="xraydawgz",
     version="0.1",
     license='MIT',
